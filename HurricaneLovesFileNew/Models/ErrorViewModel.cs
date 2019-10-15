22384091b4da9e3727d26b2c3a62d4401333b13e
Luce Carter <luce@lucyscarter.co.uk>
2019-10-15 19:18:58 +0100

    Initial Commit

---
+++ HurricaneLovesFileNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HurricaneLovesFileNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
